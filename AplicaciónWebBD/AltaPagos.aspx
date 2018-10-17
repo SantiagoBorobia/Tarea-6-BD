b95d954b229dba22763a36331203bd2c1f36a2ce
EUGENIO GARCIA RODRIGUEZ <egarciar@itam.mx>
2018-10-17 13:05:17 -0500

    3

--- AplicaciónWebBD/AltaPagos.aspx
+++ AplicaciónWebBD/AltaPagos.aspx
@@ -23,7 +23,7 @@
         <asp:Label ID="Label3" runat="server" ForeColor="White" style="z-index: 1; left: 75px; top: 156px; position: absolute" Text="Clave pedido:"></asp:Label>
         <asp:DropDownList ID="DropDownList2" runat="server" style="z-index: 1; left: 195px; top: 157px; position: absolute" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
         </asp:DropDownList>
-        <asp:Table ID="TblPagos" runat="server" style="z-index: 1; left: 484px; top: 125px; position: absolute; height: 25px; width: 468px" GridLines="Both">
+        <asp:Table ID="TblPagos" runat="server" style="z-index: 1; left: 484px; top: 125px; position: absolute; height: 25px; width: 468px" GridLines="Both" Visible="False">
             <asp:TableRow runat="server">
                 <asp:TableCell runat="server">Fecha pedido</asp:TableCell>
                 <asp:TableCell runat="server">Total pagar</asp:TableCell>
@@ -37,6 +37,19 @@
                 <asp:TableCell runat="server"></asp:TableCell>
             </asp:TableRow>
         </asp:Table>
+    
+        <asp:GridView ID="GVPagos" runat="server" style="z-index: 1; left: 487px; top: 218px; position: absolute; height: 133px; width: 474px" Visible="False">
+        </asp:GridView>
+        <asp:Button ID="BtModi" runat="server" OnClick="BtModi_Click" style="z-index: 1; left: 76px; top: 220px; position: absolute" Text="Modificaciones" />
+        <asp:DropDownList ID="DDLBaja" runat="server" style="z-index: 1; left: 71px; top: 329px; position: absolute; margin-bottom: 0px">
+        </asp:DropDownList>
+        <asp:Button ID="BtBajaP" runat="server" OnClick="BtBajaP_Click" style="z-index: 1; left: 74px; top: 270px; position: absolute; width: 90px" Text="Baja Pago" Visible="False" />
+        <asp:Button ID="BtEjecutarBaja" runat="server" OnClick="BtEjecutarBaja_Click" style="z-index: 1; left: 193px; top: 327px; position: absolute; width: 87px" Text="Dar de Baja" Visible="False" />
+        <asp:Button ID="BtAltaPago" runat="server" OnClick="BtAltaPago_Click" style="z-index: 1; left: 192px; top: 271px; position: absolute" Text="Alta Pago" Visible="False" />
+        <asp:Button ID="BtModP" runat="server" style="z-index: 1; left: 304px; top: 273px; position: absolute; width: 97px" Text="Modificar Pago" Visible="False" />
+        <asp:Label ID="LbAlta" runat="server" style="z-index: 1; left: 74px; top: 338px; position: absolute" Text="Monto" Visible="False"></asp:Label>
+        <asp:TextBox ID="TxBMonto" runat="server" style="z-index: 1; left: 183px; top: 335px; position: absolute" Visible="False"></asp:TextBox>
+        <asp:Button ID="BtEjecutarAlta" runat="server" style="z-index: 1; left: 333px; top: 333px; position: absolute" Text="Ejecutar Alta" Visible="False" />
     
     </div>
     </form>
--- AplicaciónWebBD/AltaPagos.aspx.cs
+++ AplicaciónWebBD/AltaPagos.aspx.cs
@@ -13,9 +13,10 @@ public partial class AltaPagos : System.Web.UI.Page
     GestorBD.GestorBD GestorBD;
     string rfc, cadSql;
     DataSet Dsgeneral = new DataSet(), DsPedidos = new DataSet();
-    DataSet DsArtículos = new DataSet(), DsPagos = new DataSet();
+    DataSet DsPag = new DataSet(), DsPagos = new DataSet(), DsAlta = new DataSet();
     DataRow Fila;
     Comunes común = new Comunes();
+    const int OK = 1;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -38,7 +39,7 @@ public partial class AltaPagos : System.Web.UI.Page
 
     protected void DDLNombres_SelectedIndexChanged(object sender, EventArgs e)
     {
-       
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
         //Lee los datos de los pedidos que ha hecho el empleado
         //y carga los Nombres de los clientes en el DDL.
         cadSql = "select FolioP from PCPedidos where RFCC=(select RFC from PCUsuarios where nombre='"+DDLNombres.SelectedValue+"')";
@@ -48,8 +49,79 @@ public partial class AltaPagos : System.Web.UI.Page
 
     protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
     {
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        //Muestra los otros elementos
+        TblPagos.Visible = true;
+        BtModi.Visible = true;
+        GVPagos.Visible = true;
+
+        //obtiene la informacion del pedido seleccionado y llena la tabla y el grid view
         cadSql = "select fechaPed, monto, saldocli, saldofacs from PCPedidos where folioP='" + DropDownList2.SelectedValue + "'";
         GestorBD.consBD(cadSql, DsPagos, "InfoPed");
+        Fila = DsPagos.Tables["InfoPed"].Rows[0];
+        TblPagos.Rows[1].Cells[0].Text = Fila["FechaPed"].ToString();
+        TblPagos.Rows[1].Cells[1].Text = Fila["Monto"].ToString();
+        TblPagos.Rows[1].Cells[2].Text = ((double)Fila["Monto"] - (double)Fila["SaldoCli"]).ToString();
+        TblPagos.Rows[1].Cells[2].Text = Fila["SaldoCli"].ToString();
+
+  
+        cadSql = "select IdPago, Fecha, Monto from PCPagos where FolioP = '" + DropDownList2.SelectedValue + "'";
+        GestorBD.consBD(cadSql, DsPag, "PagosPed");
+        GVPagos.DataSource = DsPag.Tables["PagosPed"];  //Muestra resultados.
+        GVPagos.DataBind();
+
+    }
+
+    protected void BtModi_Click(object sender, EventArgs e)
+    {
+        BtBajaP.Visible = true;
+        BtModi.Visible = true;
+        BtModP.Visible = true;
+    }
+
+    protected void BtBajaP_Click(object sender, EventArgs e)
+    {
+        //Agarra todos los Id´s de los pagos y los mete a un DDL
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        DDLBaja.Visible = true;
+        BtEjecutarBaja.Visible = true;
+        cadSql = "Select * from PCPagos where FolioP = '" + DropDownList2.SelectedValue + "'";
+        GestorBD.consBD(cadSql, DsPag, "Pagos");
+        común.cargaDDL(DDLBaja, DsPag, "Pagos", "IdPago");
+    }
+
+    protected void BtEjecutarBaja_Click(object sender, EventArgs e)
+    {
+        //Da de baja el pago seleccionado
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        cadSql = "delete from PCPagos where IdPago=" + DDLBaja.SelectedValue + "";
+        if (GestorBD.bajaBD(cadSql) == OK)
+            Response.Write("Eliminación exitosa en Usuarios");
+        else
+            Response.Write("Error de eliminación del Pago");
+    }
+
+    protected void BtAltaPago_Click(object sender, EventArgs e)
+    {
+        GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        LbAlta.Visible = true;
+        TxBMonto.Visible = true;
+        BtEjecutarAlta.Visible = true;
+        //Genera un Id para el nuevo pago
+        cadSql = "select max(IdPago) into MId from PCPagos";
+        GestorBD.consBD(cadSql, DsPag, "MaxId");
+        int idtemp;
+        Fila = DsPag.Tables["MaxiD"].Rows[0];
+        idtemp = (int)Fila["MId"];
+        idtemp = idtemp + 1;
+
+        //Crear el pago
+        string mon = TxBMonto.Text;
+        cadSql = "Insert into PCPagos values ('" + DropDownList2.SelectedValue + "', " + idtemp + ", " + DateTime.Now.ToString() +", "+ mon +")";
+        if (GestorBD.altaBD(cadSql) == OK)
+            Response.Write("Pago dado de Alta");
+        else
+            Response.Write("No se pudo dar de alta el pago");
 
     }
 }
